f440e584320a975cdaa3567ec2e24b3efe8aab0c
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2023-09-29 20:43:15 -0300

    Binary Calculator Completed

---
+++ binary-calculator/bin.js
@@ -0,0 +1,40 @@
+const binaryNumber = document.querySelector('#binary-number')
+const decimalNumber = document.querySelector('#decimal-number')
+const convertButton = document.querySelector('#convert-button')
+
+binaryNumber.addEventListener('change', verifyValidBinary)
+convertButton.addEventListener('click', convertBinaryToDecimal)
+
+function verifyValidBinary(){
+   let binaryValue = binaryNumber.value
+   let errors = 0
+
+   for(let i = 0; i < binaryValue.length; i++){
+      if(binaryValue[i] !== "0" && binaryValue[i] !== "1"){
+         errors++
+      }
+   }
+
+   toggleButton(errors)
+}
+
+function toggleButton(errors){
+   if(errors !== 0){
+      convertButton.disabled = true
+      convertButton.style.backgroundColor = '#ef5b5b'
+      convertButton.style.cursor = 'not-allowed'
+   }else{
+      convertButton.disabled = false
+      convertButton.style.backgroundColor = '#3cb371'
+      convertButton.style.cursor = 'pointer'
+   }
+}
+
+function convertBinaryToDecimal(){
+   let binaryValue = binaryNumber.value   
+   let convertedNumber = parseInt(binaryValue, 2)
+
+   console.log(convertedNumber)
+   decimalNumber.value = convertedNumber.toString()
+   console.log("hm")
+}
