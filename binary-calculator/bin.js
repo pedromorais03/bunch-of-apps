ecd8f894d68f103d1388a7d4addadb50d9840352
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2023-09-29 20:53:25 -0300

    Added a span for error message

--- binary-calculator/bin.js
+++ binary-calculator/bin.js
@@ -1,6 +1,7 @@
 const binaryNumber = document.querySelector('#binary-number')
 const decimalNumber = document.querySelector('#decimal-number')
 const convertButton = document.querySelector('#convert-button')
+const errorMessage = document.querySelector('.error-message')
 
 binaryNumber.addEventListener('change', verifyValidBinary)
 convertButton.addEventListener('click', convertBinaryToDecimal)
@@ -23,10 +24,14 @@ function toggleButton(errors){
       convertButton.disabled = true
       convertButton.style.backgroundColor = '#ef5b5b'
       convertButton.style.cursor = 'not-allowed'
+
+      errorMessage.style.display = 'block'
    }else{
       convertButton.disabled = false
       convertButton.style.backgroundColor = '#3cb371'
       convertButton.style.cursor = 'pointer'
+
+      errorMessage.style.display = 'none'
    }
 }
 
